ea746de6847804fce73b767e1c3eaa108a83a993
JooYoung <beachu23@naver.com>
2024-08-08 18:22:54 +0900

    feature: #7 멤버 서비스 클래스 구현
    회원가입, 회원 조회 비즈니스 로직을 지원하는 서비스 클래스를 구현한다.
    
    Fixes: #7

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.service;
+
+import java.util.List;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+
+  private final MemberRepository memberRepository;
+
+  // 회원 가입
+  @Transactional
+  public Long join(Member member) {
+    validateDuplicateMember(member);
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  private void validateDuplicateMember(Member member) {
+    // 멀티 스레드 환경 고려, DB 단에서 제약 조건을 설정하는 것도 하나의 방법
+    List<Member> findMembers = memberRepository.findByName(member.getName());
+    if (!findMembers.isEmpty()) {
+      throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+  }
+
+  // 회원 목록 조회
+  public List<Member> findMembers(){
+    return memberRepository.findAll();
+  }
+
+  public Member findOne(Long id){
+    return memberRepository.findOne(id);
+  }
+}

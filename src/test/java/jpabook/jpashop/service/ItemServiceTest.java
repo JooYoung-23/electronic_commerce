2ca35358b61ae99b1b2a91889585a203866b98b9
JooYoung <beachu23@naver.com>
2024-08-08 19:18:14 +0900

    feature: #9 상품 로직 테스트 수행
    상품 도메인 관련 클래스를 구현하고 테스트를 수행한다.
    예외처리가 정상적으로 이루어지는 지 위주로 테스트를 수행한다.
    
    Fixes: #9

--- src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -1,4 +1,25 @@
+package jpabook.jpashop.service;
+
 import static org.junit.Assert.*;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import org.junit.Test;
+
 public class ItemServiceTest {
-  
+
+  @Test(expected = NotEnoughStockException.class)
+  public void 재고_수량_변경() throws Exception {
+      //Given
+      Item book = new Book();
+      book.setStockQuantity(0);
+
+      //When
+      book.AddStock(10);
+      book.removeStock(11);
+
+      //Then
+      fail("재고 수량 부족 예외가 발생한다.");
+  }
 }

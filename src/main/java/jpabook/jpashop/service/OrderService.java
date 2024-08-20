3fb2a57722a0a9cafeef9ca0585d4073283be67e
JooYoung <beachu23@naver.com>
2024-08-20 21:52:24 +0900

    feature: #13 웹 계층 개발
    타임리프 템플릿 엔진 기반 SSR으로 웹 계층을 개발한다.
    요구사항에 맞는 기능을 제공하는 Controller를 모두 개발한다.
    
    이 과정에서 준영속 객체를 수정하는 매커니즘을 이해한다.
    Fixes: #13

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Delivery;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
@@ -24,6 +25,7 @@ public class OrderService {
   private final ItemRepository itemRepository;
 
   // 주문
+  @Transactional
   public Long order(Long memberId, Long itemId, int count) {
 
     // 엔티티 조회
@@ -54,7 +56,7 @@ public class OrderService {
   }
 
   // 검색
-//  public List<Order> findOrder(OrderSearch orderSearch) {
-//    return orderRepository.findAll(orderSearch);
-//  }
+  public List<Order> findOrders(OrderSearch orderSearch) {
+    return orderRepository.findAll(orderSearch);
+  }
 }
